50dffc0d086b0185d3d182471a59f37b257cec9c
kasmadesign <49408852+kasmadesign@users.noreply.github.com>
2019-04-10 12:21:27 +0200

    Add files via upload

---
+++ ExoPython.py
@@ -0,0 +1,9 @@
+#Saisir un nombre quelquonque n
+n =int(input("entrer un nombre"))
+
+#Déclarer quent est ce que le nombre saisie est pair
+	if n% == 0:
+		print ("Le nombre saisie par l'utilisateur est pair")
+#Déclarer quent est ce que le nombre saisie est impair
+	Also:
+		print ("Le nombre saisie par l'utilisateur est impair")
