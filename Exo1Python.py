1fc472c309c2894fdd52b71648eabe0b145f2f42
kasmadesign <49408852+kasmadesign@users.noreply.github.com>
2019-04-10 12:27:21 +0200

    Add files via upload

---
+++ Exo1Python.py
@@ -0,0 +1,12 @@
+#Déclaration des variables
+
+	valeur1 = input ("premiere note")
+	valeur2 = input("seconde note")
+
+	somme = 0
+
+	moyenne = (valeur1 + valeur2)/2
+
+	print "la moyenne est:" moyenne
+ 	
+
